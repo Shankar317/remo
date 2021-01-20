81ee6b605ec5df3b3c21390ff7a38a18cddf7c7d
Shankar317 <54468564+Shankar317@users.noreply.github.com>
2021-01-20 16:09:21 +0530

    demo

---
+++ demo.py
@@ -0,0 +1,9 @@
+from mysql import connector
+
+mydb = connector.connect(host='localhost', user='root', password='1234', database='sql_hr')
+mycursor = mydb.cursor()
+mycursor.execute('select *from employees')
+result = mycursor.fetchall()
+for i in result:
+
+ print(i)
